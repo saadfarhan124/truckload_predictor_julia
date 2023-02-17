4ad7a4820bc2431310c31a9a4e0a05448ec85329
Yasoobnaqvi <muhammadyasoobnaqvi@gmail.com>
2023-02-17 19:28:30 +0500

    fixes

--- routes.jl
+++ routes.jl
@@ -50,7 +50,7 @@ route("/hello", method=POST) do
   weightedMaxPrice = (p_df[!,:HighSpot]'*weights)/sum(weights)
   weightedDev = (p_df[!,:SpotDeviation]'*weights)/sum(weights)
 
-  return json(Dict("Min":round(weightedMinPrice,digits=2),"Median":round(weightedMedianPrice,digits=2),"Max":round(weightedMaxPrice,digits=2)))
+  return json(Dict("Min" => round(weightedMinPrice,digits=2),"Median" => round(weightedMedianPrice,digits=2),"Max" => round(weightedMaxPrice,digits=2)))
 
 end
 
